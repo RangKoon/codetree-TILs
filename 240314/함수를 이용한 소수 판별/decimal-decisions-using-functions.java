3b5c1768b0e289298c8c5dce2d2d4bf1cca0afc1
RangKoon <162738215+RangKoon@users.noreply.github.com>
2024-03-14 16:01:58 +0900

    Title: 함수를 이용한 소수 판별, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240314/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240314/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -13,7 +13,10 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = 0;
+
         for(int i=a; i<=b; i++) {
+            if(i<2)
+                continue;
             if(isPrime(i)) {
                 sum+= i;
             }
